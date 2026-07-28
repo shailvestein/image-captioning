05db3aa4ad716386f673532699cee467309e0531
shailvestein <62176093+shailvestein@users.noreply.github.com>
2026-07-28 17:49:42 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,3 +1,6 @@
+import torch
+import torch.nn as nn
+
 # Positional Encoding module to inject sequence order awareness into transformer inputs
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000):
