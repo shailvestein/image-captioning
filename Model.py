6dd786d987463f34217d50dec4d6206942e90813
shailvestein <62176093+shailvestein@users.noreply.github.com>
2026-07-28 18:02:19 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import math
-from torchvision import models, transforms
+from torchvision import models
 
 # Positional Encoding module to inject sequence order awareness into transformer inputs
 class PositionalEncoding(nn.Module):
